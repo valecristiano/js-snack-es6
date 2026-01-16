b9413e50a625ed62ff2958e0e571347b4ad76894
valecristiano <valeriamdcristiano@gmail.com>
2026-01-16 17:40:04 +0300

    snack 2 random numbers

--- script.js
+++ script.js
@@ -36,3 +36,52 @@ for (let i = 0; i < bicycles.length; i++) {
   }
 }
 console.log("Bici più leggera:", lighterBicycle);
+
+// Snack2
+
+// Creare un array di oggetti di squadre di calcio.
+
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti
+// e stampiamo tutto in console.
+
+// Creare un array di squadre di calcio con proprietà: nome, punti fatti, falli subiti, dove
+// Nome sarà l’unica proprietà da compilare
+
+const teams = [
+  {
+    nome: "Napoli",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Cagliari",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Juventus",
+    punti: 0,
+    falli: 0,
+  },
+  {
+    nome: "Roma",
+    punti: 0,
+    falli: 0,
+  },
+];
+console.table(teams);
+
+for (let i = 0; i < teams.length; i++) {
+  const currentBicycle = teams[i];
+  currentBicycle.punti += RandomNumbers(0, 100);
+  currentBicycle.falli += RandomNumbers(0, 100);
+}
+console.table(teams);
+
+function RandomNumbers(min, max) {
+  let number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
+}
